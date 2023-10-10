86dccf3743e905b0b96a531d9485a34726cc8725
Mouton <mouton@edenya.net>
2023-10-10 12:45:13 +0200

    ESO update 39

--- src/BreakReminder.lua
+++ src/BreakReminder.lua
@@ -6,7 +6,7 @@ BreakReminder = {
         registerForRefresh = true,
         author = "|c0cccc0@mouton|r",
         recipient = "@mouton",
-        version = "1.0.0",
+        version = "1.0.1",
         website = "https://www.esoui.com/downloads/info3431-BreakReminderTimerampMemos.html",
         max_reminders = 10
     },
